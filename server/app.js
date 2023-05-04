d0b4007e1f74b09485a045346895777fc3a8ac79
Zimmer Kennedy <kennedyzimmer00@gmail.com>
2023-05-04 09:15:59 -0800

    JWT-ts

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require("cors");
+require('dotenv').config();
 module.exports = app
 
 // logging middleware
